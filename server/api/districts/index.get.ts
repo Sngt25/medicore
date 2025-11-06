1458167c49f61439fa6d50855b05ee7ea04e136d
Sngt25 <jeddycertifico25@gmail.com>
2025-11-06 18:22:26 +0800

    feat: add query handling for fetching a specific district by ID

--- server/api/districts/index.get.ts
+++ server/api/districts/index.get.ts
@@ -1,4 +1,17 @@
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  const id = query.id as string | undefined
+
+  if (id) {
+    const district = await useDrizzle()
+      .select()
+      .from(tables.districts)
+      .where(eq(tables.districts.id, id))
+      .get()
+
+    return district
+  }
+
   const districts = await useDrizzle()
     .select()
     .from(tables.districts)
